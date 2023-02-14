d62ecccf686ec571eab981238ee462083e412b09
Dan Hancox <dthancox@gmail.com>
2023-02-14 21:33:14 +0000

    Add property button

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "../styles/add-property.css";
 
 const AddProperty = () => {
-  return <div className="add-property">Add Property Page</div>;
+  return (
+    <div className="add-property">
+      <form>
+        <button className="submit-button" type="submit">
+          Add Property
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddProperty;
