5afeae74938fae59d0ac7d0305ccdfab30730c94
Dan Hancox <dthancox@gmail.com>
2023-02-20 10:35:16 +0000

    Option fields, select type and location

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -5,13 +5,16 @@ const AddProperty = () => {
   const initialState = {
     fields: {
       title: "",
+      city: "Manchester",
     },
   };
   const [fields, setFields] = useState(initialState.fields);
+
   const handleAddProperty = (event) => {
     event.preventDefault();
     console.log(fields);
   };
+
   const handleFieldChange = (event) => {
     setFields({ ...fields, [event.target.name]: event.target.value });
   };
@@ -27,6 +30,34 @@ const AddProperty = () => {
             value={fields.title}
             onChange={handleFieldChange}
           />
+        </label>
+        <label htmlFor="city">
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+          </select>
+        </label>
+        <label htmlFor="type">
+          Type:
+          <select
+            id="type"
+            name="type"
+            value={fields.type}
+            onChange={handleFieldChange}
+          >
+            <option value="Flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">Semi-Deteched</option>
+            <option value="Terraced">Terraced</option>
+            <option value="End of Terrace">End of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+          </select>
         </label>
         <button className="submit-button" type="submit">
           Add Property
