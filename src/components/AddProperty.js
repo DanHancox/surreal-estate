c607254d900e878a54795821aa5ac0d9ae68c202
Dan Hancox <dthancox@gmail.com>
2023-02-20 10:22:10 +0000

    handle functions added, input field and console logs

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -7,9 +7,27 @@ const AddProperty = () => {
       title: "",
     },
   };
+  const [fields, setFields] = useState(initialState.fields);
+  const handleAddProperty = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  };
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
   return (
     <div className="add-property">
-      <form>
+      <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">
+          Title
+          <input
+            id="title"
+            name="title"
+            value={fields.title}
+            onChange={handleFieldChange}
+          />
+        </label>
         <button className="submit-button" type="submit">
           Add Property
         </button>
